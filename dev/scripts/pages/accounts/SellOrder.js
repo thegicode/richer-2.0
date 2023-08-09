e81bbacdf6a193114f724808fefd675b12d40bd3
thegicode <thegi.code@gmail.com>
2023-08-09 21:54:22 +0900

    update accounts: add memo, add sellPrice options

--- dev/scripts/pages/accounts/SellOrder.js
+++ dev/scripts/pages/accounts/SellOrder.js
@@ -29,39 +29,111 @@ class SellOrder {
                 this.askButton.remove();
                 return;
             }
-            this.askButton.addEventListener("click", this.onClick.bind(this));
+            this.askButton.addEventListener("click", this.show.bind(this));
         });
     }
-    onClick() {
+    show() {
         var _a;
         const template = (_a = this.template) === null || _a === void 0 ? void 0 : _a.content.firstElementChild;
         const element = template === null || template === void 0 ? void 0 : template.cloneNode(true);
-        this.renderOrder(element);
-        this.removeOrder(element);
+        this.render(element);
+        this.addEvent(element);
+        this.remove(element);
         this.parentElement.appendChild(element);
     }
-    renderOrder(element) {
-        return __awaiter(this, void 0, void 0, function* () {
-            if (!this.data)
-                return;
-            this.askButton.disabled = true;
-            const data = this.data;
-            const { ask_account, } = data;
-            const { currency, balance, unit_currency, } = ask_account;
-            const askPrice = this.avgBuyPrice + this.avgBuyPrice * 0.1;
-            element.querySelector(".orderAvailable .value").textContent =
-                balance.toString();
-            element.querySelector(".sellPrice input").value =
-                askPrice.toString();
-            element.querySelectorAll("dl .unit").forEach((el) => {
-                el.textContent = unit_currency;
-            });
-            element.querySelectorAll(".market-unit").forEach((el) => {
-                el.textContent = currency;
+    render(element) {
+        if (!this.data)
+            return;
+        this.askButton.disabled = true;
+        const data = this.data;
+        const { ask_fee, market, ask_account } = data;
+        const { currency, balance, unit_currency, } = ask_account;
+        const { ask } = market;
+        element.querySelectorAll("dl .unit").forEach((el) => {
+            el.textContent = unit_currency;
+        });
+        element.querySelectorAll(".market-unit").forEach((el) => {
+            el.textContent = currency;
+        });
+        element.querySelector(".orderAvailable .value").textContent =
+            balance.toString();
+        element.querySelector(".sellOrder-memo .minTotal .value").textContent =
+            ask.min_total;
+        element.querySelector(".sellOrder-memo .fee .value").textContent =
+            ask_fee;
+    }
+    addEvent(element) {
+        var _a, _b;
+        if (!this.data)
+            return;
+        const { balance } = this.data.ask_account;
+        const sellPriceInput = element.querySelector(".sellPrice input");
+        const orderQuantityInput = element.querySelector(".orderQuantity input");
+        const totalOrderAmountInput = element.querySelector(".totalOrderAmount input");
+        const sellPriceRadios = element.querySelectorAll("input[name='sellPrice-percent']");
+        const submitButton = element.querySelector("button[type='submit']");
+        const validate = () => {
+            if (sellPriceInput.value &&
+                orderQuantityInput.value &&
+                totalOrderAmountInput.value)
+                submitButton.disabled = false;
+            else
+                submitButton.disabled = true;
+        };
+        const fromSellPriceToTotalOrderAmount = () => {
+            if (orderQuantityInput.value) {
+                totalOrderAmountInput.value = (Number(sellPriceInput.value) *
+                    Number(orderQuantityInput.value)).toString();
+            }
+            validate();
+        };
+        sellPriceInput.value = this.tradePrice.toString();
+        sellPriceInput.addEventListener("input", () => {
+            fromSellPriceToTotalOrderAmount();
+        });
+        orderQuantityInput.addEventListener("input", () => {
+            const quantity = Number(orderQuantityInput.value);
+            if (quantity < balance) {
+                const result = quantity * Number(sellPriceInput.value);
+                totalOrderAmountInput.value = Math.round(result).toString();
+            }
+            else {
+                alert("주문 가능 수량 초과입니다. ");
+                orderQuantityInput.value = "";
+            }
+            validate();
+        });
+        totalOrderAmountInput.addEventListener("input", () => {
+            const quantity = Number(totalOrderAmountInput.value) /
+                Number(sellPriceInput.value);
+            if (quantity < balance) {
+                orderQuantityInput.value = quantity.toString();
+            }
+            else {
+                alert("주문 가능 수량 초과입니다. ");
+                orderQuantityInput.value = "";
+            }
+            validate();
+        });
+        sellPriceRadios.forEach((radio) => {
+            radio.addEventListener("change", () => {
+                const { checked, value } = radio;
+                const rate = Number(value);
+                if (checked) {
+                    const price = this.tradePrice + this.tradePrice * rate;
+                    sellPriceInput.value = price.toString();
+                    fromSellPriceToTotalOrderAmount();
+                }
             });
         });
+        (_a = element.querySelector("form")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", (event) => {
+            event.preventDefault();
+        });
+        (_b = element.querySelector("form")) === null || _b === void 0 ? void 0 : _b.addEventListener("reset", () => {
+            submitButton.disabled = true;
+        });
     }
-    removeOrder(element) {
+    remove(element) {
         const closeButton = element === null || element === void 0 ? void 0 : element.querySelector(".closeButton");
         closeButton === null || closeButton === void 0 ? void 0 : closeButton.addEventListener("click", () => {
             this.parentElement.removeChild(element);
--- src/scripts/pages/accounts/SellOrder.ts
+++ src/scripts/pages/accounts/SellOrder.ts
@@ -29,40 +29,36 @@ class SellOrder {
 
     private async iniitialize() {
         const marketName = this.market;
-
         const data = await fetchData("/getChance", marketName);
         this.data = data;
-        const { balance } = data.ask_account;
 
+        const { balance } = data.ask_account;
         if (Number(balance) === 0) {
             this.askButton.remove();
             return;
         }
 
-        this.askButton.addEventListener("click", this.onClick.bind(this));
+        this.askButton.addEventListener("click", this.show.bind(this));
     }
 
-    onClick() {
+    private show() {
         const template = this.template?.content.firstElementChild;
         const element = template?.cloneNode(true) as HTMLElement;
 
-        this.renderOrder(element);
-        this.removeOrder(element);
+        this.render(element);
+        this.addEvent(element);
+        this.remove(element);
 
         this.parentElement.appendChild(element);
     }
 
-    private async renderOrder(element: HTMLElement) {
+    private render(element: HTMLElement) {
         if (!this.data) return;
 
         this.askButton.disabled = true;
 
         const data = this.data;
-        const {
-            // ask_fee,
-            // market,
-            ask_account,
-        } = data;
+        const { ask_fee, market, ask_account } = data;
         const {
             currency,
             balance,
@@ -71,17 +67,7 @@ class SellOrder {
             // avg_buy_price_modified,
             unit_currency,
         } = ask_account;
-
-        const askPrice = this.avgBuyPrice + this.avgBuyPrice * 0.1;
-
-        element.querySelector(".orderAvailable .value")!.textContent =
-            balance.toString();
-        // element.querySelector(".orderAvailable .unit")!.textContent =
-        //     unit_currency;
-        (element.querySelector(".sellPrice input") as HTMLInputElement).value =
-            askPrice.toString();
-        // orderQuantity
-        // totalOrderAmount
+        const { ask } = market;
 
         element.querySelectorAll("dl .unit").forEach((el) => {
             el.textContent = unit_currency;
@@ -89,9 +75,113 @@ class SellOrder {
         element.querySelectorAll(".market-unit").forEach((el) => {
             el.textContent = currency;
         });
+
+        element.querySelector(".orderAvailable .value")!.textContent =
+            balance.toString();
+
+        element.querySelector(".sellOrder-memo .minTotal .value")!.textContent =
+            ask.min_total;
+        element.querySelector(".sellOrder-memo .fee .value")!.textContent =
+            ask_fee;
     }
 
-    private removeOrder(element: HTMLElement) {
+    private addEvent(element: HTMLElement) {
+        if (!this.data) return;
+        const { balance } = this.data.ask_account;
+
+        const sellPriceInput = element.querySelector(
+            ".sellPrice input"
+        ) as HTMLInputElement;
+        const orderQuantityInput = element.querySelector(
+            ".orderQuantity input"
+        ) as HTMLInputElement;
+        const totalOrderAmountInput = element.querySelector(
+            ".totalOrderAmount input"
+        ) as HTMLInputElement;
+        const sellPriceRadios: NodeListOf<HTMLInputElement> =
+            element.querySelectorAll("input[name='sellPrice-percent']");
+
+        const submitButton = element.querySelector(
+            "button[type='submit']"
+        ) as HTMLButtonElement;
+        // const resetButton = element.querySelector(
+        //     "button[type='reset']"
+        // ) as HTMLButtonElement;
+
+        const validate = () => {
+            if (
+                sellPriceInput.value &&
+                orderQuantityInput.value &&
+                totalOrderAmountInput.value
+            )
+                submitButton.disabled = false;
+            else submitButton.disabled = true;
+        };
+
+        const fromSellPriceToTotalOrderAmount = () => {
+            if (orderQuantityInput.value) {
+                totalOrderAmountInput.value = (
+                    Number(sellPriceInput.value) *
+                    Number(orderQuantityInput.value)
+                ).toString();
+            }
+            validate();
+        };
+
+        sellPriceInput.value = this.tradePrice.toString();
+
+        sellPriceInput.addEventListener("input", () => {
+            fromSellPriceToTotalOrderAmount();
+        });
+
+        orderQuantityInput.addEventListener("input", () => {
+            const quantity = Number(orderQuantityInput.value);
+            if (quantity < balance) {
+                const result = quantity * Number(sellPriceInput.value);
+                totalOrderAmountInput.value = Math.round(result).toString();
+            } else {
+                alert("주문 가능 수량 초과입니다. ");
+                orderQuantityInput.value = "";
+            }
+            validate();
+        });
+
+        totalOrderAmountInput.addEventListener("input", () => {
+            const quantity =
+                Number(totalOrderAmountInput.value) /
+                Number(sellPriceInput.value);
+            if (quantity < balance) {
+                orderQuantityInput.value = quantity.toString();
+            } else {
+                alert("주문 가능 수량 초과입니다. ");
+                orderQuantityInput.value = "";
+            }
+            validate();
+        });
+
+        sellPriceRadios.forEach((radio) => {
+            radio.addEventListener("change", () => {
+                const { checked, value } = radio;
+                const rate = Number(value);
+                if (checked) {
+                    const price = this.tradePrice + this.tradePrice * rate;
+                    sellPriceInput.value = price.toString();
+
+                    fromSellPriceToTotalOrderAmount();
+                }
+            });
+        });
+
+        element.querySelector("form")?.addEventListener("submit", (event) => {
+            event.preventDefault();
+        });
+
+        element.querySelector("form")?.addEventListener("reset", () => {
+            submitButton.disabled = true;
+        });
+    }
+
+    private remove(element: HTMLElement) {
         const closeButton = element?.querySelector(".closeButton");
         closeButton?.addEventListener("click", () => {
             this.parentElement.removeChild(element);
